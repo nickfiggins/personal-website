190b7414cec32d9c46d574a4f15c2fc8e1f4a621
Nick <figginsn@gmail.com>
2020-07-10 16:12:22 -0400

    ayyy

--- src/Components/About.js
+++ src/Components/About.js
@@ -21,8 +21,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={myImage} alt="Nick Figgins Profile Pic" />
-            {profilepic}
+            <img className="profile-pic"  src={profilepic} alt="Nick Figgins Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
