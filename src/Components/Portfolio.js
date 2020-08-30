76ecb3270f66b6e0a6f5c4db720457c5c2cdef5e
Nick <figginsn@gmail.com>
2020-08-30 13:35:12 -0400

    updated projects/contact

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-
+import './Portfolio.css';
 class Portfolio extends Component {
   render() {
 
     if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
+      var main_project = this.props.data.projects.slice(0,1);
+      var presenter = main_project.map(function(projects){
+        var projectImage = 'images/portfolio/'+projects.image;
+        return <div key={projects.title} className="columns portfolio-item">
+           <div className="item-wrap">
+            <a href={projects.url} title={projects.title}>
+               <img alt={projects.title} src={projectImage}/>
+               <div className="overlay">
+                  <div className="portfolio-item-meta">
+                 <h5>{projects.title}</h5>
+                     <p>{projects.category}</p>
+                  </div>
+                </div>
+              <div className="link-icon"><i className="fa fa-link"></i></div>
+            </a>
+          </div>
+        </div>
+      })
+      console.log(presenter);
+      var projects = this.props.data.projects.slice(1,).map(function(projects){
         var projectImage = 'images/portfolio/'+projects.image;
         return <div key={projects.title} className="columns portfolio-item">
            <div className="item-wrap">
@@ -31,7 +50,12 @@ class Portfolio extends Component {
          <div className="twelve columns collapsed">
 
             <h1>Check Out Some of My Works.</h1>
-
+            <div id="presenter-wrapper" className="bgrid-quarters s-bgrid-thirds cf" style={{paddingBottom: '50px'}}>
+            {presenter} <p> <b>Spotipie.com</b><p>
+            Website created by myself and <a href="http://github.com/willyptrain/">Will Peterson</a> using React.js and Flask. This application leverages the Spotify
+            Web API to provide graphs, recommendations, and various other information about a user's music listening history. 
+            </p> </p>
+            </div>
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
             </div>
